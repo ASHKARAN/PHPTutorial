715e15fdb21315156db80ad7ffb60b67a80d823a
ali <ali@goldriverpoker.com>
2019-08-17 21:43:27 +0430

    03-Functions file names changed

---
+++ 03-Functions.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+//echo "hi";
+
+
+
+for($i = 0 ; $i<10 ; $i++) {
+  //  printName($i );
+}
+
+
+printName("ali" , "ghalandari");
+
+function printName($fname , $lname = "ashkaran" ) {
+
+    echo "$fname $lname\n";
+
+}
+
