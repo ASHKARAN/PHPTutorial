cf189d39d549842832584144de8c55da73698a8f
ASHKARAN <ali.ashkaran@gmail.com>
2021-02-20 23:22:36 +0300

    16-Users-Login-1

--- miniShop/api/v1/Controller/Users.php
+++ miniShop/api/v1/Controller/Users.php
@@ -2,21 +2,30 @@
 namespace miniShop\Controller;
 use miniShop\App;
 use miniShop\Model\UserModel;
-use miniShop\MyPDO;
 
 class Users
 {
 
+    private $userModel;
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+    }
 
     public function Register($json) {
         App::hasKeys($json, ["email", "password"]);
-        $userModel = new UserModel();
-        $user = $userModel->createUser($json->email, $json->password);
+
+        $user = $this->userModel->createUser($json->email, $json->password);
         App::out($user, 200);
     }
 
     public function Login($json) {
             App::hasKeys($json, ["email", "password"]);
+            $user = $this->userModel->login($json->email, $json->password);
+            if($user == null) {
+                App::out("User not found with this email and password");
+            }
+            App::out($user, 200);
     }
 
 
--- miniShop/api/v1/Model/UserModel.php
+++ miniShop/api/v1/Model/UserModel.php
@@ -11,7 +11,7 @@ class UserModel
     public function createUser($email, $password) {
 
 
-        $user =  MyPDO::doSelect("SELECT * FROM users WHERE email = ? ",
+        $user =  MyPDO::doSelect("SELECT userID FROM users WHERE email = ? ",
             [$email], false, false);
 
         if($user != null) {
@@ -19,12 +19,18 @@ class UserModel
         }
 
 
-        $password = (sha1( Config::$PASSWORD_SALT . $password));
+        $password = md5(sha1( Config::$PASSWORD_SALT . $password));
         MyPDO::doQuery("INSERT INTO users (email, password) VALUES (? ,? )",
         [$email, $password]);
 
         return MyPDO::doSelect("SELECT userID, email FROM users WHERE userID = ? ",
-        [MyPDO::getLastInsertId(MyPDO::getInstance())]);
+        [MyPDO::getLastInsertId(MyPDO::getInstance())], false, false);
 
+    }
+    public function login($email, $password) {
+
+        $password = md5(sha1( Config::$PASSWORD_SALT . $password));
+       return  MyPDO::doSelect("SELECT userID , email FROM users WHERE email = ? AND password = ? ",
+            [$email, $password], false, false);
     }
 }
